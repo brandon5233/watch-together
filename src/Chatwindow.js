9a4b4d0e0b83f1897f827d552e0beefa1ee51150
Brandon Rozario <rozario.brandon@gmail.com>
2018-12-20 15:28:48 +0000

    initial chat window

---
+++ src/Chatwindow.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './Chatwindow.css'
+
+class Chatwindow extends Component{
+    render(){
+        return(
+            <div className="Chatwindow">
+                <p> chat window goes here </p>
+            </div>
+        );
+    }
+}
+
+export default Chatwindow;
