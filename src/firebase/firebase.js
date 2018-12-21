b2fb2af74ead3ff67e653e71230b3a509e6c0915
Brandon Rozario <rozario.brandon@gmail.com>
2018-12-21 17:09:37 +0000

    set up firebase context and functions

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,5 @@
 import app from 'firebase/app';
-
+import 'firebase/auth';
 const config = {
     apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -13,7 +13,25 @@ const config = {
   class Firebase{
       constructor(){
           app.initializeApp(config);
+          this.auth = app.auth();
       }
+
+      // Auth Api
+    doCreateUserWithEmailAndPassword=(email, password) =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+    dosignInWithEmailAndPassword=(email, password) =>
+        this.auth.signInWithEmailAndPassword(email, password);
+    
+    doSignOut=()=>this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password =>
+      this.auth.currentUser.updatePassword(password);
+
+
+
   }
 
   export default Firebase;
